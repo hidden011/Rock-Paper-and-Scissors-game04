43cf632a756dea9540f387fd2566849a998e1076
hidden011 <way1rajesh@gmail.com>
2025-08-24 21:55:16 +0530

    script.js

--- script.js
+++ script.js
@@ -2,25 +2,66 @@ let choices = document.body.querySelector(".choices");
 let player = document.body.querySelector(".player")
 let computer_Text = document.body.querySelector(".computer");
 let result_out= document.body.querySelector(".result");
+function computer() {
+        let arrChoices = ["🗿", "🧻", "✂️"];
+        let ranIndex = Math.floor(Math.floor((Math.random()*10))/4);
+        computer_Text.innerHTML = `COMPUTER: ${arrChoices[ranIndex]}`;
+        return arrChoices[ranIndex];
+    }
+   
+function result(comp, play) {
+        if(comp == play) {
+            result_out.textContent = "It's a Tie 😐";
+            result_out.style.backgroundColor = "grey";
 
+        }
+        else if(comp ==`🗿` && play ==`🧻` ) {
+            result_out.textContent = "You win! 😃";
+            result_out.style.backgroundColor = "lightgreen";
+
+        }
+
+        else if(comp ==`🧻` && play ==`🗿` ) {
+            result_out.textContent = "You Lost!! 😔";
+            result_out.style.backgroundColor = "#FF7F7F";
+
+        }
+
+        else if(comp ==`🗿` && play ==`✂️` ) {
+            result_out.textContent = "You Lost!! 😔";
+            result_out.style.backgroundColor = "#FF7F7F";
+           
+
+        }
+
+        else if(comp ==`✂️` && play ==`🗿` ) {
+            result_out.textContent = "You win! 😃";
+            result_out.style.backgroundColor = "lightgreen";
+
+        }
+
+        else if(comp ==`✂️` && play ==`🧻` ) {
+             result_out.textContent = "You Lost!! 😔";
+            result_out.style.backgroundColor = "#FF7F7F";
+
+        }
+
+        else if(comp ==`🧻` && play ==`✂️` ) {
+            result_out.textContent = "You win! 😃";
+            result_out.style.backgroundColor = "lightgreen";
+
+        }
+  
+    }
 
 choices.addEventListener('click', function(e) {
     
-    let comp_choice = '';
+    let comp_choice = computer();
     let player_choice = '';
-    function computer() {
-        let arrChoices = ["🪨", "🧻", "✂️"];
-        let ranIndex = Math.floor(Math.floor((Math.random()*10))/4);
-        computer_Text.innerHTML = `COMPUTER: ${arrChoices[ranIndex]}`;
-        comp_choice = arrChoices[ranIndex];
-        
-        
-    }
-computer();
     
     if(e.target.className == "rock") {
-        player.innerHTML = "PLAYER: 🪨";
-        player_choice = "🪨";
+        player.innerHTML = "PLAYER: 🗿";
+        player_choice = "🗿";
 
     }
     else if(e.target.className == 'paper') {
@@ -33,25 +74,8 @@ computer();
         player_choice = "✂️";
 
     }
-    result(comp_choice, player_choice) ;
-    function result(comp, play) {
-        if(comp == play) {
-            result_out.textContent = "You Win!! 😃";
-            result_out.style.backgroundColor = "lightgreen";
-
-        }
-        else{
-            result_out.textContent = "You Lost!! 😔";
-            result_out.style.backgroundColor = "#FF7F7F";
-            result_out.style.color = "white";
-            
-
-        }
-    }
-        
-
     
-
+    result(comp_choice, player_choice);
 });
 
 
